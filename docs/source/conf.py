efa6467b58bb668e04bde99fc874ca686bc485fe
Heanan Bird <138244718+heananbird@users.noreply.github.com>
2023-07-11 10:41:31 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Cost Throughput Curve User Guide'
+copyright = '2023'
+author = 'Seeq'
 
 release = '0.1'
 version = '0.1.0'
